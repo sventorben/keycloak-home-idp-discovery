959c150f095f596bdf2535da7aadbd8108aaca73
Sven-Torben Janus <sven-torben.janus@conciso.de>
2021-11-24 23:12:57 +0100

    fix: Fix some issues with UI

--- src/main/resources/theme/home-idp-discovery/admin/resources/js/home-idp-discovery/controllers.js
+++ src/main/resources/theme/home-idp-discovery/admin/resources/js/home-idp-discovery/controllers.js
@@ -1,15 +1,8 @@
-module.controller('HomeIdpDiscoveryConfigCtrl', function($scope, realm, homeIdpDiscoveryConfig, Notifications, $route, Dialog) {
+module.controller('HomeIdpDiscoveryConfigCtrl', function($scope, realm, homeIdpDiscoveryConfig, identityProvider, Notifications, $route, Dialog) {
 
     $scope.realm = realm;
     $scope.homeIdpDiscoveryConfig = homeIdpDiscoveryConfig
-
-    $scope.page = 0;
-
-    $scope.query = {
-        realm: realm.realm,
-        max: 20,
-        first: 0
-    };
+    $scope.identityProvider = identityProvider
 
     $scope.searchQuery = function() {
         $scope.searchLoaded = false;
@@ -17,20 +10,6 @@ module.controller('HomeIdpDiscoveryConfigCtrl', function($scope, realm, homeIdpD
         $scope.details = HomeIdpDiscoveryConfig.query($scope.query, function() {
             $scope.searchLoaded = true;
             $scope.lastSearch = $scope.query.search;
-        });
-    };
-
-    $scope.removeBeer = function(beer) {
-        Dialog.confirmDelete(beer.name, 'beer', function() {
-            beer.$remove({
-                realm : realm.realm,
-                beerId : beer.id
-            }, function() {
-                $route.reload();
-                Notifications.success("Beer has been deleted.");
-            }, function() {
-                Notifications.error("Beer couldn't be deleted");
-            });
         });
     };
 
