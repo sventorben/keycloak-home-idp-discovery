71b89d0b6caacf25e0c42d94c408781458267b9b
Sven-Torben Janus <sven-torben.janus@conciso.de>
2021-12-10 10:23:34 +0100

    chore: Remove unused code from Redirector

--- src/main/java/de/sventorben/keycloak/authentication/hidpd/Redirector.java
+++ src/main/java/de/sventorben/keycloak/authentication/hidpd/Redirector.java
@@ -1,11 +1,8 @@
 package de.sventorben.keycloak.authentication.hidpd;
 
 import org.jboss.logging.Logger;
-import org.keycloak.OAuth2Constants;
 import org.keycloak.authentication.AuthenticationFlowContext;
-import org.keycloak.authentication.AuthenticationProcessor;
 import org.keycloak.broker.provider.AuthenticationRequest;
-import org.keycloak.broker.provider.IdentityBrokerException;
 import org.keycloak.broker.provider.IdentityProvider;
 import org.keycloak.broker.provider.IdentityProviderFactory;
 import org.keycloak.broker.provider.util.IdentityBrokerState;
@@ -16,21 +13,15 @@ import org.keycloak.models.RealmModel;
 import org.keycloak.protocol.oidc.OIDCLoginProtocol;
 import org.keycloak.services.Urls;
 import org.keycloak.services.managers.ClientSessionCode;
-import org.keycloak.services.messages.Messages;
 import org.keycloak.sessions.AuthenticationSessionModel;
 
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-import java.net.URI;
 
 import static org.keycloak.services.resources.IdentityBrokerService.getIdentityProviderFactory;
 
 final class Redirector {
 
     private static final Logger LOG = Logger.getLogger(Redirector.class);
-
-    private static final String PROMPT_NONE = "none";
-    private static final String ACCEPTS_PROMPT_NONE = "acceptsPromptNoneForwardFromClient";
 
     private final AuthenticationFlowContext context;
 
@@ -78,41 +69,4 @@ final class Redirector {
         return new AuthenticationRequest(keycloakSession, realm, authSession, context.getHttpRequest(), keycloakUriInfo, encodedState, redirectUri);
     }
 
-    /*
-    void redirectTo(IdentityProviderModel idp) {
-        URI baseUri = context.getUriInfo().getBaseUri();
-        String providerId = idp.getAlias();
-        String realm = context.getRealm().getName();
-        AuthenticationSessionModel authenticationSession = context.getAuthenticationSession();
-        String accessCode = new ClientSessionCode<>(context.getSession(), context.getRealm(), authenticationSession)
-            .getOrGenerateCode();
-        String clientId = authenticationSession.getClient().getClientId();
-        String tabId = authenticationSession.getTabId();
-        URI location = Urls.identityProviderAuthnRequest(baseUri, providerId, realm, accessCode, clientId, tabId);
-        UriBuilder redirectUriBuilder = UriBuilder.fromUri(location);
-        if (authenticationSession.getClientNote(OAuth2Constants.DISPLAY) != null) {
-            redirectUriBuilder = redirectUriBuilder
-                .queryParam(OAuth2Constants.DISPLAY, authenticationSession.getClientNote(OAuth2Constants.DISPLAY));
-        }
-        String loginHint = context.getAuthenticationSession().getClientNote(OIDCLoginProtocol.LOGIN_HINT_PARAM);
-        if (idp.isLoginHint() && loginHint != null) {
-            redirectUriBuilder = redirectUriBuilder
-                .queryParam(OIDCLoginProtocol.LOGIN_HINT_PARAM, loginHint);
-        }
-        String uiLocales = context.getHttpRequest().getUri().getQueryParameters().getFirst(OAuth2Constants.UI_LOCALES_PARAM);
-        if (uiLocales != null) {
-            uiLocales = uiLocales.trim();
-            if (uiLocales.length() > 0) {
-                redirectUriBuilder.queryParam(OAuth2Constants.UI_LOCALES_PARAM, uiLocales);
-            }
-        }
-        Response response = Response.seeOther(redirectUriBuilder.build()).build();
-        if (PROMPT_NONE.equals(authenticationSession.getClientNote(OIDCLoginProtocol.PROMPT_PARAM)) &&
-            Boolean.parseBoolean(idp.getConfig().get(ACCEPTS_PROMPT_NONE))) {
-            authenticationSession.setAuthNote(AuthenticationProcessor.FORWARDED_PASSIVE_LOGIN, "true");
-        }
-        LOG.debugf("Redirecting user %s of reealm %s to IdP %s", context.getUser().getUsername(), realm, providerId);
-        context.forceChallenge(response);
-    }
-    */
 }
