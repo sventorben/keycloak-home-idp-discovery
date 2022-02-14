fafa01ac34fe7ef79e8a2a828a2f5c74095dd514
Sven-Torben Janus <sven-torben.janus@conciso.de>
2022-02-14 13:00:16 +0100

    chore: Fix compatibility tests with Keycloak.X latest

--- src/test/java/de/sventorben/keycloak/authentication/HomeIdpDiscoveryIT.java
+++ src/test/java/de/sventorben/keycloak/authentication/HomeIdpDiscoveryIT.java
@@ -76,7 +76,7 @@ class HomeIdpDiscoveryIT {
         if ("keycloak-x".equalsIgnoreCase(KEYCLOAK_DIST) &&
             !"latest".equalsIgnoreCase(KEYCLOAK_VERSION) && Version.parse(KEYCLOAK_VERSION).compareTo(Version.parse("15.1")) < 0) {
             container = new KeycloakXContainer(fullImage, KEYCLOAK_VERSION);
-        } else if ("keycloak-x".equalsIgnoreCase(KEYCLOAK_DIST)) {
+        } else if ("keycloak-x".equalsIgnoreCase(KEYCLOAK_DIST) || ("keycloak".equalsIgnoreCase(KEYCLOAK_DIST) && "latest".equalsIgnoreCase(KEYCLOAK_VERSION))) {
             container = new KeycloakXContainer(fullImage, KEYCLOAK_VERSION);
         } else {
             container = new KeycloakContainer(fullImage);
