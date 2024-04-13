dc1f8cb8e5297962c2931ecb825b7957e5d18dc4
Sven-Torben Janus <sven-torben@sven-torben.de>
2024-04-13 11:42:47 +0200

    docs: Document unstable SPI with annotation

--- src/main/java/de/sventorben/keycloak/authentication/hidpd/discovery/spi/HomeIdpDiscovererFactory.java
+++ src/main/java/de/sventorben/keycloak/authentication/hidpd/discovery/spi/HomeIdpDiscovererFactory.java
@@ -2,5 +2,6 @@ package de.sventorben.keycloak.authentication.hidpd.discovery.spi;
 
 import org.keycloak.provider.ProviderFactory;
 
+@Unstable
 public interface HomeIdpDiscovererFactory extends ProviderFactory<HomeIdpDiscoverer> {
 }
