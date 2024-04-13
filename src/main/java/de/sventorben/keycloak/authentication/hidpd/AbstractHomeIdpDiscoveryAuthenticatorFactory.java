b11829f1f6cf97fb70d8c312ee8d3f6080c037ce
Sven-Torben Janus <sven-torben@sven-torben.de>
2024-04-13 13:03:30 +0200

    docs: Extended java docs for authenticator factory

--- src/main/java/de/sventorben/keycloak/authentication/hidpd/AbstractHomeIdpDiscoveryAuthenticatorFactory.java
+++ src/main/java/de/sventorben/keycloak/authentication/hidpd/AbstractHomeIdpDiscoveryAuthenticatorFactory.java
@@ -1,5 +1,6 @@
 package de.sventorben.keycloak.authentication.hidpd;
 
+import de.sventorben.keycloak.authentication.hidpd.discovery.spi.HomeIdpDiscoverer;
 import org.keycloak.Config;
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.authentication.AuthenticatorFactory;
@@ -16,6 +17,27 @@ import java.util.stream.Stream;
 
 import static org.keycloak.models.AuthenticationExecutionModel.Requirement.*;
 
+/**
+ * Provides a base implementation for authenticator factories that integrate custom identity provider
+ * discovery mechanisms within authentication flow of this extension. This abstract class simplifies
+ * the creation of authenticator instances by encapsulating common logic and providing a framework
+ * for extending the discovery functionality through custom {@link HomeIdpDiscoverer} implementations.
+ * <p>
+ * Implementors of this class need to provide their own {@link DiscovererConfig}, which includes
+ * the discovery logic specifics and configuration properties. This approach ensures flexibility and
+ * customizability, enabling developers to tailor the identity provider discovery process to specific
+ * organizational needs or authentication scenarios.
+ * </p>
+ * <p>
+ * By inheriting from this class, developers can focus on the specifics of their discovery logic
+ * without worrying about the boilerplate associated with UI integration and redirection logic.
+ * </p>
+ *
+ * @apiNote This interface is part of the public API, but is currently unstable and may change in future releases.
+ *
+ * @see HomeIdpDiscoverer
+ * @see DiscovererConfig
+ */
 @PublicAPI(unstable = true)
 public abstract class AbstractHomeIdpDiscoveryAuthenticatorFactory implements AuthenticatorFactory, ServerInfoAwareProviderFactory {
     private static final AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = new AuthenticationExecutionModel.Requirement[]{REQUIRED, ALTERNATIVE, DISABLED};
@@ -75,9 +97,39 @@ public abstract class AbstractHomeIdpDiscoveryAuthenticatorFactory implements Au
         return Map.of("Version", version);
     }
 
+    /**
+     * Represents the configuration settings for a {@link HomeIdpDiscoverer} implementation. This interface
+     * is designed to allow for dynamic specification of configuration properties necessary for the
+     * discovery of home Identity Providers (IdPs). The configurations defined by an implementation of
+     * this interface provide the parameters and metadata required by a discoverer to properly integrate
+     * with {@link HomeIdpDiscoveryAuthenticator}.
+     *
+     * @apiNote This interface is part of the public API, but is currently unstable and may change in future releases.
+     *
+     * @see HomeIdpDiscoverer
+     */
+    @PublicAPI(unstable = true)
     public interface DiscovererConfig {
+        /**
+         * Retrieves a list of {@link ProviderConfigProperty} objects that define the configuration
+         * properties available for the discoverer. Each {@code ProviderConfigProperty} includes metadata
+         * such as the property name, type, label, default value, and other attributes necessary for
+         * configuring the discoverer identified by {@link #getProviderId()} dynamically at runtime.
+         *
+         * @return a list of {@link ProviderConfigProperty} that describes each configuration property
+         *         required by the discoverer. If no home properties are need for configuration, this method must
+         *         return an empty list.
+         */
         List<ProviderConfigProperty> getProperties();
 
+        /**
+         * Returns the unique provider ID associated with the discoverer. This ID is used to uniquely
+         * identify and reference the specific discoverer implementation within the Keycloak system.
+         * The provider ID should be unique across all discoverer configurations to prevent conflicts
+         * and ensure correct operation.
+         *
+         * @return the unique string identifier for the discoverer provider.
+         */
         String getProviderId();
     }
 }
--- src/main/java/de/sventorben/keycloak/authentication/hidpd/discovery/spi/HomeIdpDiscovererFactory.java
+++ src/main/java/de/sventorben/keycloak/authentication/hidpd/discovery/spi/HomeIdpDiscovererFactory.java
@@ -3,6 +3,9 @@ package de.sventorben.keycloak.authentication.hidpd.discovery.spi;
 import de.sventorben.keycloak.authentication.hidpd.PublicAPI;
 import org.keycloak.provider.ProviderFactory;
 
+/**
+ * @apiNote This interface is part of the public API, but is currently unstable and may change in future releases.
+ */
 @PublicAPI(unstable = true)
 public interface HomeIdpDiscovererFactory extends ProviderFactory<HomeIdpDiscoverer> {
 }
