51ad4e9d985d94b184e709e3fac3f55c1919750f
Sven-Torben Janus <sven-torben@sven-torben.de>
2024-12-04 10:10:26 +0100

    fix: Prevent NPE when AuthenticatorConfig is null
    Closes #465
    
    Signed-off-by: Sven-Torben Janus <sven-torben@sven-torben.de>

--- src/main/java/de/sventorben/keycloak/authentication/hidpd/AuthenticationChallenge.java
+++ src/main/java/de/sventorben/keycloak/authentication/hidpd/AuthenticationChallenge.java
@@ -8,6 +8,7 @@ import org.keycloak.models.IdentityProviderModel;
 import org.keycloak.services.managers.AuthenticationManager;
 
 import java.util.List;
+import java.util.Optional;
 
 final class AuthenticationChallenge {
 
@@ -32,7 +33,9 @@ final class AuthenticationChallenge {
         String rememberMeUsername = rememberMe.getUserName();
 
         if (reauthentication.required() && context.getUser() != null) {
-            String attribute = context.getAuthenticatorConfig().getConfig().getOrDefault("userAttribute", "username");
+            String attribute = Optional.ofNullable(context.getAuthenticatorConfig())
+                .map(it -> it.getConfig().getOrDefault("userAttribute", "email").trim())
+                .orElse("email");
             formData.add(AuthenticationManager.FORM_USERNAME, context.getUser().getFirstAttribute(attribute));
         } else {
             if (loginHintUsername != null || rememberMeUsername != null) {
